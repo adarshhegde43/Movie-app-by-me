117491cecee71f5bf13dc8a0679e9f6e8493774d
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-04-17 16:56:12 +0530

    2 - added MovieListHeading and SearchBox components .

--- src/App.js
+++ src/App.js
@@ -4,30 +4,42 @@ import 'bootstrap/dist/css/bootstrap.min.css' ; //importing bootstrap...
 import './App.css'; //importing our own styles...
 
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies , setMovies ] = useState([]) ;
+  const [searchValue , setSearchValue] = useState('') ; 
 
-    const getMovieRequest = async () => {
-      const url = "http://www.omdbapi.com/?s=star wars&apikey=5638c325"; //hardcoded url currently....
 
-      const response = await fetch(url) ; 
-      const responseJSON = await response.json() ; 
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=5638c325"; //hardcoded url currently....
 
-      console.log(responseJSON) ; 
+    const response = await fetch(url) ; 
+    const responseJSON = await response.json() ; 
 
-      setMovies(responseJSON.Search) ; 
-    }
+    console.log(responseJSON) ; 
 
-    useEffect(() => {
-      getMovieRequest() ; 
-    } , []) ; 
+    setMovies(responseJSON.Search) ; 
+  }
+
+  useEffect(() => {
+    getMovieRequest() ; 
+  } , []) ; 
 
   return (
+
       <div className='container-fluid movie-app'>
+
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+          <MovieListHeading heading = 'Movies'/>
+          <SearchBox/>
+        </div>
+
         <div className='row'>
           <MovieList movies={movies} />
         </div>
+
       </div>
   );  
 }
